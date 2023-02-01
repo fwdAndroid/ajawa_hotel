83fa9281a8c75aea8d5cce5779bd1cb579f51dab
fwdAndroid <fwdkaleem@gmail.com>
2023-02-02 03:10:08 +0500

    Basic Setup

---
+++ lib/dashhboard_screens/resturant_management.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Resturant_Management extends StatefulWidget {
+  const Resturant_Management({super.key});
+
+  @override
+  State<Resturant_Management> createState() => _Resturant_ManagementState();
+}
+
+class _Resturant_ManagementState extends State<Resturant_Management> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
